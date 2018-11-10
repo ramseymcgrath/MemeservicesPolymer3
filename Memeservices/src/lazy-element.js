37de4794cf7484f4e4e54f61af791435ca236b2f
Ramsey McGrath <ramseymcgrath@gmail.com>
2018-11-10 14:46:00 -0800

    Adding randomization to string

--- Memeservices/src/lazy-element.js
+++ Memeservices/src/lazy-element.js
@@ -15,12 +15,14 @@ import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 
 class LazyElement extends PolymerElement {
   static get template () {
+    var memearray = ["https://firebasestorage.googleapis.com/v0/b/memeservices-4c685.appspot.com/o/Unforgivable1.PNG?alt=media&token=e96688cb-a925-4bcc-ab8a-1296dfdcb17f","https://firebasestorage.googleapis.com/v0/b/memeservices-4c685.appspot.com/o/Blank%2B_4589ee8edadc094016dbc864001b63f2.png?alt=media&token=afb0154b-4542-44cb-aed7-14fb1ab378f3","https://firebasestorage.googleapis.com/v0/b/memeservices-4c685.appspot.com/o/fff.jpg?alt=media&token=af58af86-81fe-4098-85e0-4e628e63adbb","https://firebasestorage.googleapis.com/v0/b/memeservices-4c685.appspot.com/o/pasted_image_at_2017_12_07_11_29_pm.png?alt=media&token=6cad2833-1128-43ab-a647-37dcb2ee83de"];
     // Template getter must return an instance of HTMLTemplateElement.
     // The html helper function makes this easy.
-    return html`
-      <p>You like memes.</p>
-      <img src="https://firebasestorage.googleapis.com/v0/b/memeservices-4c685.appspot.com/o/Unforgivable1.PNG?alt=media&token=e96688cb-a925-4bcc-ab8a-1296dfdcb17f" style="border: none; width: 100%; alt="Unforgivable">
-    `;
+    var randomNumber = Math.floor(Math.random()*memearray.length);
+
+    var memestring = memearray[randomNumber];
+    var outputstring = "<p>You like memes.</p> <img src=" +  memestring  + "style=border: none; width: 100%; alt='Unforgivable'>";
+    return HTMLFormElement(outputstring);
   }
 }
 
